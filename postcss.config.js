9f35eaa557fc0aa6f865e63faa1a5e9fe6775c61
Thien Nguyen <tatthien.contact@gmail.com>
2019-05-27 11:28:25 +0700

    Add: purgecss to reduce the css file size

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  plugins: [tailwindcss],
+  plugins: [
+    tailwindcss,
+    process.env.NODE_ENV === 'production' ? purgecss({
+      content: [
+        './src/**/*.vue',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }) : [],
+  ],
 };
