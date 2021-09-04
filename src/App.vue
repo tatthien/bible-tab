ad106e914816c7a0ec68de889ff04ea569a76a20
Thien Nguyen <tatthien.contact@gmail.com>
2021-09-04 14:31:25 +0700

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ msg }}</div>
+  <div>{{ test }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {
+      msg: 'Hello',
+      test: '123',
+    }
+  },
+})
+</script>
